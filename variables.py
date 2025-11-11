4e4b630be7d3318a06ad9ad6d288d7b26942902f
0xvantrex <callagansikuche0@gmail.com>
2025-11-11 10:38:14 +0300

    add variables.py

---
+++ variables.py
@@ -0,0 +1,21 @@
+# a variable is  a container that stores a value
+# const name = "Callagan" javascript
+# $name = "callagan" php
+#val name= "callagan" kotlin
+#name = "vantrex" python
+
+jina = " vantrex global"
+
+print (" i learn coding at ", jina)
+print (" i learn coding at "+ jina)
+print (f" i learn coding at {jina}")
+
+
+num1 =8
+num2 =9
+print (f"The sum of {num1} and {num2} is {num1+num2}")
+#create for product, difference, quotient, remainder(%)
+print (f"The product of {num1} and {num2} is {num1+num2}")
+print (f"The difference of {num1} and {num2} is {num1-num2}")
+print (f"The quotient of {num1} and {num2} is {num1/num2}")
+print (f"The remainder of {num1} and {num2} is {num1%num2}")
