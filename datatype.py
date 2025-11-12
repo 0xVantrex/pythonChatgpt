81f6b8216e90e3f20108cf0da6ec22ac17664a32
0xvantrex <callagansikuche0@gmail.com>
2025-11-12 15:12:39 +0300

    add datatype.py

---
+++ datatype.py
@@ -0,0 +1,16 @@
+my_string="callagan"
+my_int=64
+my_float=74.65
+my_bool=True
+print(type(my_string))
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+
+my_list=["michael", "jane", "john", "mercy"]
+my_tuple=("michael", "jane", "john", "mercy")
+my_set={"michael", "jane", "john", "mercy"}
+my_dict={"name":"michael",
+         "age":18,
+         "location":"nairobi"
+         }
