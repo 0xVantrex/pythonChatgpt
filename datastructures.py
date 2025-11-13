463b91b7c0ad6e4ed1a0bf76bf4bb2e60c3ba1c8
0xvantrex <callagansikuche0@gmail.com>
2025-11-13 21:36:50 +0300

    add datastructures.py

---
+++ datastructures.py
@@ -0,0 +1,24 @@
+#list datastructures
+fruits=["banana", "mangoes", "oranges", "apples"]
+fruits.sort()
+print(fruits)
+fruits[1]="pineapple"
+print(f"I like eating {fruits[1]}")
+
+# list is ordered
+# it has indexes
+# its changeable
+
+# tuple datastructures
+cars = ("mercedes", "bmw", "lamborghini", "Nissan", "subaru")
+print(cars)
+print (f"{cars[0]} are manufactured in Berlin")
+
+#tuples are ordered
+# it has indexes
+# its unchageable
+
+#set datastrucutres
+majina={"callagan", "darius", "shamilla", "sharonne", "zipporah"}
+
+
