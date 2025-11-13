4dcda06ad5ae90c3fbbe6cda4e1b80b4fa55c2ea
0xvantrex <callagansikuche0@gmail.com>
2025-11-13 21:36:05 +0300

    add functions.py"

---
+++ functions.py
@@ -0,0 +1,10 @@
+def myfunction():
+    print("this is my first function")
+myfunction()
+
+# function with parameters
+def students(name,age,gender):
+    print(f"Student na is a {name} ")
+    print(f"Student age is  {age}")
+    print(f"student gender is {gender}")
+students("callagan",43,"male")
