85d2d3bbd273604447e82dd3adefc87b05121bea
0xvantrex <callagansikuche0@gmail.com>
2025-11-13 21:33:20 +0300

    add loops.py

---
+++ loops.py
@@ -0,0 +1,24 @@
+# while loop
+number=0
+
+while number<=15:
+    print(f"Loop: {number}")
+    number+=2
+
+#for loop
+deepseekclass=["cael","lety","naaman","trisha","shanon"]
+print(deepseekclass)
+for x in deepseekclass:
+    print(x)
+
+myname="callagan"
+for i in myname:
+    print(i)
+
+#sum of numbers in a list
+numbers=[10,24,17,25,6,64,76,31,14,654,1]
+total=0
+for num in numbers:
+    total+=num
+print (f"the sum of the numbers in {total}")
+
