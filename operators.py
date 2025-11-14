849db3cbdfb73c6b6251fd048627c56e4f8ed0ea
0xvantrex <callagansikuche0@gmail.com>
2025-11-14 19:16:52 +0300

    update operators.py

--- operators.py
+++ operators.py
@@ -1,8 +1,23 @@
 #arithmetic operators
 x=7
 y=3
-print(x+y)
-print(x-y)
-print(x*y)
-print(x/y)
-print(x%y)
+print(x+y) #addition
+print(x-y) #subtraction
+print(x*y) #multiplication
+print(x/y) #division
+print(x%y) #remainder
+print(x**y) #exponentiaition
+print(x//y)  #floor division
+
+#comparison operators
+print (x==y) #equal to
+print (x!=y) #not equal to
+print ( x > y) #greater than
+print (x< y) # less than
+print ( x <= y) #less than or equal to
+print ( x>= y) # greater thatn or equal to
+
+#logical operators
+print (x > 5 and y < 10 ) # both should be true
+print (x > 5  or y < 10 ) # one should be true
+print(not(x>5 and y < 10)) 
