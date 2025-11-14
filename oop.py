49dc41192d21945667589cc0dbd9417f239814ee
0xvantrex <callagansikuche0@gmail.com>
2025-11-14 19:16:04 +0300

    update oop.py

---
+++ oop.py
@@ -0,0 +1,9 @@
+class Student:
+
+    # define a constructor
+    def __init__(self,name,score):
+        self.name=name
+        self.score=score
+        #define a method
+        def display(self):
+            print(f"Name: {self.name} Score: {self.score}")
